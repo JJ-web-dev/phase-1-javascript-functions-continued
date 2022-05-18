6e1f296ed0f82ca0b2f6b7ee1fc7ec928710c268
Justin James <justinmjames@protonmail.com>
2022-05-17 18:15:08 -0700

    commit tree 8189e39b9a5531c8ae49ef0c5521b7767157362b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+};
+
+saturdayFun();
+
+const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+mondayWork();
+
+function wrapAdjective(highlight = '*'){
+    return function(say = 'hard worker' ){
+        return `You are ${highlight}${say}${highlight}!`;
+    }
+}     
+
+wrapAdjective();
